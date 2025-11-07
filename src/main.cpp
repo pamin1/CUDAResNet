0041fb14e0de6bb1d70746c43bccad2dede6a341
Prachit Amin <prachitamin12@gmail.com>
2025-11-07 16:52:10 -0500

    Compilation with model parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include "ImageClassifier.h"
+#include "ModelParse.h"
 
-int main() {
-    // create an image classifier object
-    ImageClassifier ic("images/dog.png");
+int main()
+{
+  // create an image classifier object
+  // ImageClassifier ic("assets/dog.png");
 
-    // grab the host image
-    std::vector<float> hostImage = ic.getHostImage();
-    if (hostImage.size() != 0) {
-        std::cout << "successful host image grab\n";
-    }
-    
-    // begin CUDA allocation
-    
-    return 0;
+  // grab the host image
+  // std::vector<float> hostImage = ic.getHostImage();
+  // if (hostImage.size() != 0) {
+  //     std::cout << "successful host image grab\n";
+  // }
+
+  // parse model json
+  ModelParse mp("assets/resnet18_manifest.json");
+  json m = mp.getModel();
+
+  ResNet18 model = mp.generateModel();
+  mp.printResNet18(model);
+
+  return 0;
 }

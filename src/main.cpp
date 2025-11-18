528a8b6520570eafe00547826744bb15b078c5d5
Prachit Amin <prachitamin12@gmail.com>
2025-11-18 14:52:43 -0500

    Removed previous debugging

--- src/main.cpp
+++ src/main.cpp
@@ -3,64 +3,6 @@
 #include "Kernel.cuh"
 #include "ModelParse.h"
 
-void printWeightStatistics(const float *d_weight, int out_channels, int in_channels,
-                           int kernel_height, int kernel_width)
-{
-  int total_weights = out_channels * in_channels * kernel_height * kernel_width;
-
-  float *h_weight = (float *)malloc(total_weights * sizeof(float));
-  cudaMemcpy(h_weight, d_weight, total_weights * sizeof(float), cudaMemcpyDeviceToHost);
-
-  printf("\n=== Weight Statistics ===\n");
-  printf("Total weights: %d\n", total_weights);
-
-  int weights_per_channel = in_channels * kernel_height * kernel_width;
-
-  for (int oc = 0; oc < out_channels; oc++)
-  {
-    float min_val = h_weight[oc * weights_per_channel];
-    float max_val = h_weight[oc * weights_per_channel];
-    float sum = 0.0f;
-    int zero_count = 0;
-
-    for (int i = 0; i < weights_per_channel; i++)
-    {
-      int idx = oc * weights_per_channel + i;
-      float val = h_weight[idx];
-
-      if (val < min_val)
-        min_val = val;
-      if (val > max_val)
-        max_val = val;
-      if (val == 0.0f)
-        zero_count++;
-      sum += val;
-    }
-
-    printf("Ch %2d: min=%8.4f, max=%8.4f, mean=%8.4f, zeros=%d/%d (%.1f%%)\n", oc, min_val, max_val,
-           sum / weights_per_channel, zero_count, weights_per_channel,
-           100.0f * zero_count / weights_per_channel);
-  }
-
-  free(h_weight);
-}
-
-void dumpWeightsRaw(const float *d_weight, int total_weights, const char *filename)
-{
-  float *h_weight = (float *)malloc(total_weights * sizeof(float));
-  cudaMemcpy(h_weight, d_weight, total_weights * sizeof(float), cudaMemcpyDeviceToHost);
-
-  FILE *fp = fopen(filename, "w");
-  for (int i = 0; i < total_weights; i++)
-  {
-    fprintf(fp, "%.8f\n", h_weight[i]);
-  }
-  fclose(fp);
-  free(h_weight);
-
-  printf("Dumped %d weights to %s\n", total_weights, filename);
-}
-
 int main()
 {
   // create an image classifier object
@@ -109,29 +51,6 @@ int main()
   // copy the output back
   float *res = (float *)malloc(outSize * sizeof(float));
   CHECK_ERROR(cudaMemcpy(res, out, outSize * sizeof(float), cudaMemcpyDeviceToHost));
-
-  // try to see the data?
-  int count = 0;
-  int i = 0;
-  // std::ofstream outfile("output.txt", std::ios::app);
-  // for (int i = 0; i < outSize; i++)
-  // {
-  //   outfile << res[i] << "\n";
-  //   count++;
-  // }
-  // outfile.close();
-  std::cout << "Count: " << count << "\n";
-
-  printf("Channel 4 weights [0:10]: ");
-  for (int i = 0; i < 10; i++)
-  {
-    printf("%f ", model.conv1.h_weight[4 * dModel.conv1.inputSize * dModel.conv1.kernelSize *
-                                           dModel.conv1.kernelSize +
-                                       i]);
-  }
-  printf("\n");
-
-  dumpWeightsRaw(dModel.conv1.d_weight, 64 * 3 * 7 * 7, "weights.txt");
 
   return 0;
 }

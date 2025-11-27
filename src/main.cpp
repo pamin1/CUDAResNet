9c471568a6196cb2eaf7e3464a0de0a5f39a18b5
Prachit Amin <prachitamin12@gmail.com>
2025-11-27 16:44:11 -0500

    Added benchmarking

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,76 @@
-#include "Benchmark.h"
-#include "ImageClassifier.h"
+#include "ImageParse.h"
 #include "Kernel.cuh"
 #include "ModelImplementation.h"
 #include "ModelParse.h"
-#include <labels.h>
+#include <chrono>
+#include <fmt/format.h>
 
-#define SAMPLE 1000
+constexpr int sampleSize = 1000;
+constexpr int imgSize = 224 * 224 * 3;
+constexpr size_t byteSize = imgSize * sizeof(float);
 
 int main()
 {
     // parse model
     ModelParse mp("assets/resnet18_manifest.json", "assets/resnet18_fp32.npz");
     ResNet18 model = mp.generateModel();
-
     mp.printResNet18(model);
 
-    // initialize CIFAR
-    CIFARLoader cifar("assets/cifar-10-batches-bin/test_batch.bin", SAMPLE);
-
-    // SETUP: Preload all images to GPU (do this ONCE, before benchmarking)
-    std::cout << "Loading images to GPU...\n";
-    std::vector<float *> d_images;
-    std::vector<float *> h_results_buffers;
-
-    for (int i = 0; i < SAMPLE; i++)
+    // load images onto cpu
+    std::vector<float> h_images(imgSize * sampleSize);
+    for (int i = 0; i < 1000; i++)
     {
-        float *h_image = cifar.getProcessedImage(i);
+        std::string path = fmt::format("assets/cifar10/images/image_{:04d}.png", i);
+        ImageParse im = ImageParse(path);
 
-        float *d_image;
-        CHECK_ERROR(cudaMalloc((void **)&d_image, 224 * 224 * 3 * sizeof(float)));
-        cudaMemcpy(d_image, h_image, 224 * 224 * 3 * sizeof(float), cudaMemcpyHostToDevice);
+        float *h_image = im.getHostImage();
 
-        d_images.push_back(d_image);
-        h_results_buffers.push_back(new float[1000]);
-
-        delete[] h_image;
+        std::memcpy(&h_images[i * imgSize], h_image, byteSize);
     }
+    std::cout << "Loaded images\n";
 
-    std::cout << "All images loaded to GPU\n";
+    // copy over to gpu
+    float *d_images;
+    CHECK_ERROR(cudaMalloc((void **)&d_images, byteSize * sampleSize));
+    cudaMemcpy(d_images, h_images.data(), byteSize * sampleSize, cudaMemcpyHostToDevice);
 
-    // WARMUP
-    std::cout << "Warming up...\n";
+    // gpu warmup
     for (int i = 0; i < 10; i++)
     {
-        float *res = launchModel(model, d_images[i % d_images.size()]);
-        delete[] res;
+        float *conf = launchModel(model, d_images + (i * imgSize));
     }
 
-    // BENCHMARK (pure inference, no I/O)
-    std::cout << "Running benchmark...\n";
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
-    cudaEventRecord(start);
-
-    for (int i = 0; i < SAMPLE; i++)
+    // run benchmarking
+    double totalTime = 0;
+    for (int i = 0; i < 1000; i++)
     {
-        float *res = launchModel(model, d_images[i]);
-        delete[] res;
+        const auto start = std::chrono::high_resolution_clock::now();
+        float *conf = launchModel(model, d_images + (i * imgSize));
+        int predicted_class = 0;
+        float max_score = conf[0];
+        for (int i = 1; i < 1000; i++)
+        {
+            if (conf[i] > max_score)
+            {
+                max_score = conf[i];
+                predicted_class = i;
+            }
+        }
+        const auto end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double, std::milli> duration = end - start;
+        totalTime += duration.count();
+
+        // uncomment for inference verification
+        // std::cout << "Class: " << predicted_class << ",\tConfidence: " << max_score << ",\tTime: " << duration.count() << "\n";
     }
 
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-
-    float total_ms;
-    cudaEventElapsedTime(&total_ms, start, stop);
-
-    std::cout << "\n=== BENCHMARK RESULTS (Pure Inference) ===\n";
-    std::cout << "Images: SAMPLE\n";
-    std::cout << "Total time: " << total_ms << " ms\n";
-    std::cout << "Average per image: " << total_ms / SAMPLE << " ms\n";
-    std::cout << "Throughput: " << (SAMPLE * 1000.0f) / total_ms << " img/s\n";
-
-    // CLEANUP
-    for (auto d_img : d_images)
-        cudaFree(d_img);
-    for (auto h_res : h_results_buffers)
-        delete[] h_res;
+    // total timing metrics
+    std::cout << "Total Time: " << totalTime << " ms\n"
+              << "Average Time: " << totalTime / sampleSize << " ms/img\n"
+              << "Average Freq: " << sampleSize / totalTime << " Hz\n";
 
     // cleanup
     mp.freeModel(model);
-
+    cudaFree(d_images);
     return 0;
 }
